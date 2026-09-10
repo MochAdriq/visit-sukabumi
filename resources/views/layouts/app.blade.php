a27ad17389735a87b2b8e053de7c7bec773da5c2
Moch Adriq <mochfadillah1208@gmail.com>
2026-09-10 17:16:33 +0700

    Fix: Force bust favicon browser cache

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <title>{{ config('app.name', 'Visit Sukabumi') }}</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=2">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,600,700,900" rel="stylesheet" />
