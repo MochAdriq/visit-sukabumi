fa5e67a66d59890f3a3cb7b8370bfabd0bf82b97
Moch Adriq <mochfadillah1208@gmail.com>
2026-09-10 11:52:51 +0700

    feat(frontend): change rating filter icons from circles to stars

--- resources/views/place/index.blade.php
+++ resources/views/place/index.blade.php
@@ -74,7 +74,9 @@
                                         <input type="checkbox" class="w-4 h-4 text-[#00aa6c] focus:ring-[#00aa6c] border-gray-300 rounded">
                                         <span class="flex items-center gap-1">
                                             @for($i=1; $i<=5; $i++)
-                                                <svg class="w-3.5 h-3.5 {{ $i <= $star ? 'text-[#00aa6c]' : 'text-gray-200' }} fill-current" viewBox="0 0 20 20"><circle cx="10" cy="10" r="8"/></svg>
+                                                <svg class="w-4 h-4 {{ $i <= $star ? 'text-[#00aa6c]' : 'text-gray-200' }} fill-current" viewBox="0 0 20 20">
+                                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
+                                                </svg>
                                             @endfor
                                             <span class="text-xs text-gray-500 ml-1">& ke atas</span>
                                         </span>
